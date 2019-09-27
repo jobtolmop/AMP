f01e2bd22dfc04f0407484144dd61c5ff5bd20ab
Jobau <jobtolmop@gmail.com>
2019-09-27 14:30:19 +0200

    Yes

--- html/04draggablePoint/script.js
+++ html/04draggablePoint/script.js
@@ -7,6 +7,14 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let point = new Point(new Vector2d(200 ,200),20, "blue");
+let point = new Point(new Vector2d(200 ,200),20, "blue", true);
 
-point.draw(context);
+
+
+function animate()
+{
+  context.clearRect(0,0,width,height);
+  requestAnimationFrame(animate);
+  point.draw(context);
+}
+animate()
