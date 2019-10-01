b39311e9130efae9548d3fbd771c11b99a010df3
Jobau <jobtolmop@gmail.com>
2019-10-01 14:35:07 +0200

    yes

--- html/05drawTheLine/script.js
+++ html/05drawTheLine/script.js
@@ -7,8 +7,8 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(200,200),15,true);
-let B = new Point(new Vector2d(500,300),15,true);
+let A = new Point(new Vector2d(200,200),15,"green",true);
+let B = new Point(new Vector2d(500,300),15,"green",true);
 
 
 let l = new LinearFunction(1/3, 133.33);
@@ -18,7 +18,7 @@ l.defineLineByTwoPoints(A,B);
 function animate()
 {
   requestAnimationFrame(animate);
-  context.clearRect(0,0,width,length);
+  context.clearRect(0,0,width,height);
   for (let x = 0; x < width; x+=10)
   {
     let P = new Point(new Vector2d(x,l.calcY(x)),1);
