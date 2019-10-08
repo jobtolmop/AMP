3029725d68a942f7b9d84c1e8237061a3bfe099d
Jobau <jobtolmop@gmail.com>
2019-10-08 13:16:56 +0200

    very epic

--- html/03connectTheDots/script.js
+++ html/03connectTheDots/script.js
@@ -7,43 +7,44 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let dots = [];
-for (var i = 0; i <4; i++) {
-  addPoint();
+let points = [];
+
+let mouseVector = new Vector2d(0,0);
+
+for (let i = 0; i < 4; i++)
+{
+  points[i] = new Point(new Vector2d(getRandom(width),getRandom(height)),20,"rgb(3, 252, 182)", i);
 }
 
-function animate(){
-  context.clearRect(0,0,canvas.width,canvas.height)
+function animate()
+{
+  context.clearRect(0,0,width,height);
   requestAnimationFrame(animate);
-  if(Math.random()<0.01){
-      dots.splice(0,1)
-      addPoint();
-  }
 
-  context.fillStyle = "rgba(255,255,0,0.2)"
   context.beginPath();
-  context.moveTo(dots[0].pos,dots[0].y);
-  for (var i = 0; i < dots.length; i++) {
-    context.lineWidth = 2;
-    context.lineTo(dots[i].x,dots[i].y);
+
+  context.moveTo(points[0].position.dx,points[0].position.dy);
+
+  for (let i = 0; i < points.length; i++)
+  {
+    context.lineTo(points[i].position.dx,points[i].position.dy)
   }
 
   context.closePath();
+  context.fillStyle="grey";
   context.fill();
+  context.strokeStyle="black";
+  context.lineWidth = 5;
   context.stroke();
-  for (var i = 0; i < dots.length; i++) {
-    dots[i].draw(context);
-    // dots[i].printText(i);
+  for (let i = 0; i < points.length; i++)
+  {
+    points[i].draw(context);
   }
 }
 
 animate();
 
-function randomNumber(max){
-  return Math.random()*max;
-}
-
-function addPoint(){
-  let dot = new Point(randomNumber(canvas.width),randomNumber(canvas.height),30,"#" + Math.floor(randomNumber(255*255*255)).toString(16));
-  dots.push(dot);
+function getRandom(max)
+{
+  return Math.floor(Math.random()*max);
 }
