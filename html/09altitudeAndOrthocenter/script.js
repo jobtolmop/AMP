3f59462b067e16e15029b525c863d5f1a93b527c
Jobau <jobtolmop@gmail.com>
2019-10-15 13:45:31 +0200

    Task 9 Done

--- html/09altitudeAndOrthocenter/script.js
+++ html/09altitudeAndOrthocenter/script.js
@@ -7,60 +7,46 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let A = new Point(new Vector2d(750,100), 20, "red", true);
-let B = new Point(new Vector2d(1100,600), 20, "red", true);
-let C = new Point(new Vector2d(400,600), 20, "red", true);
-
-let D = new Point(new Vector2d(475,350), 20, "orange", false);
-let E = new Point(new Vector2d(225,450), 20, "orange", false);
-let F = new Point(new Vector2d(750,500), 20, "orange", false);
-
-let S = new Point(new Vector2d(200,500), 15, "white", true);
+let  A = new Point(new Vector2d(100,120), 20, 255,0,0,true);
+let  B = new Point(new Vector2d(500,100), 20, 0,0,255,true);
+let  C = new Point(new Vector2d(300,500), 20, 0,255,0,true);
+let  S = new Point(new Vector2d(0,0), 10, 200,195,0,true);
 
 let l = new LinearFunction(1,1);
+let k = new LinearFunction(1,1);
 let m = new LinearFunction(1,1);
-let n = new LinearFunction(1,1);
+let l2 = new LinearFunction(1,1);
+let k2 = new LinearFunction(1,1);
+let m2 = new LinearFunction(1,1);
 
-let o = new LinearFunction(1,1);
-let p = new LinearFunction(1,1);
-let q = new LinearFunction(1,1);
-
-
-function animate()
-{
-  requestAnimationFrame(animate);
-  context.clearRect(0, 0, width, height);
+function animate(){
+  window.requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
 
   l.defineLineByTwoPoints(A,B);
-  m.defineLineByTwoPoints(B,C);
-  n.defineLineByTwoPoints(A,C);
+  m.defineLineByTwoPoints(C,B);
+  k.defineLineByTwoPoints(C,A);
+
+  l2.slope = -(1/m.slope);
+  l2.intercept = A.position.dy - l2.slope * A.position.dx;
+  m2.slope = -(1/k.slope);
+  m2.intercept = B.position.dy - m2.slope * B.position.dx;
+  k2.slope = -(1/l.slope);
+  k2.intercept = C.position.dy - k2.slope * C.position.dx;
 
   l.draw(context);
+  l2.draw(context);
   m.draw(context);
-  n.draw(context);
+  m2.draw(context);
+  k.draw(context);
+  k2.draw(context);
 
-  o.defineLineByTwoPoints(S,D);
-  p.defineLineByTwoPoints(S,E);
-  q.defineLineByTwoPoints(S,F);
-
-  o.draw(context);
-  p.draw(context);
-  q.draw(context);
-
+  C.draw(context);
   A.draw(context);
   B.draw(context);
-  C.draw(context);
-  
-  D.MiddleLine(A,B);
-  D.draw(context);
 
-  E.MiddleLine(B,C);
-  E.draw(context);
-
-  F.MiddleLine(A,C);
-  F.draw(context);
-
-  S.MiddleCrossPoint(A,B,C);
+  S.position.dx = (m2.intercept-l2.intercept)/(l2.slope-m2.slope);
+  S.position.dy = (l2.slope*S.position.dx)+l2.intercept;
   S.draw(context);
 }
 
