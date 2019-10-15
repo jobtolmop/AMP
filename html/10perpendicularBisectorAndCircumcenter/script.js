367437c48a2d00cb2e253f5ca5fb7ab65efde57e
Jobau <jobtolmop@gmail.com>
2019-10-15 14:22:39 +0200

    Task 10 Done

--- html/10perpendicularBisectorAndCircumcenter/script.js
+++ html/10perpendicularBisectorAndCircumcenter/script.js
@@ -7,46 +7,55 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let  A = new Point(new Vector2d(100,120), 20, 255,0,0,true);
-let  B = new Point(new Vector2d(500,100), 20, 0,0,255,true);
-let  C = new Point(new Vector2d(300,500), 20, 0,255,0,true);
-let  S = new Point(new Vector2d(0,0), 10, 200,195,0,true);
+let  A = new Point(new Vector2d(100,110), 15, 255,0,0,true);
+let  B = new Point(new Vector2d(500,100), 15, 255,0,0,true);
+let  C = new Point(new Vector2d(300,500), 15, 255,0,0,true);
+let  D = new Point(new Vector2d(0,0), 5, 100,100,100,false);
+let  E = new Point(new Vector2d(0,0), 5, 100,100,100,false);
+let  F = new Point(new Vector2d(0,0), 5, 100,100,100,false);
+let  S = new Point(new Vector2d(0,0), 5, 200,200,200,false);
 
 let l = new LinearFunction(1,1);
-let k = new LinearFunction(1,1);
 let m = new LinearFunction(1,1);
-let l2 = new LinearFunction(1,1);
-let k2 = new LinearFunction(1,1);
-let m2 = new LinearFunction(1,1);
+let n = new LinearFunction(1,1);
+let o = new LinearFunction(1,1);
+let p = new LinearFunction(1,1);
+let q = new LinearFunction(1,1);
 
-function animate(){
+function animate()
+{
   window.requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
 
   l.defineLineByTwoPoints(A,B);
   m.defineLineByTwoPoints(C,B);
-  k.defineLineByTwoPoints(C,A);
+  n.defineLineByTwoPoints(C,A);
 
-  l2.slope = -(1/m.slope);
-  l2.intercept = A.position.dy - l2.slope * A.position.dx;
-  m2.slope = -(1/k.slope);
-  m2.intercept = B.position.dy - m2.slope * B.position.dx;
-  k2.slope = -(1/l.slope);
-  k2.intercept = C.position.dy - k2.slope * C.position.dx;
+  o.calcPerp(D,l);
+  p.calcPerp(F,m);
+  q.calcPerp(E,n);
 
   l.draw(context);
-  l2.draw(context);
   m.draw(context);
-  m2.draw(context);
-  k.draw(context);
-  k2.draw(context);
+  n.draw(context);
+  o.draw(context);
+  p.draw(context);
+  q.draw(context);
 
-  C.draw(context);
+  D.calcMiddle(A,B);
+  E.calcMiddle(A,C);
+  F.calcMiddle(B,C);
+
   A.draw(context);
   B.draw(context);
+  C.draw(context);
+  D.draw(context);
+  E.draw(context);
+  F.draw(context);
 
-  S.position.dx = (m2.intercept-l2.intercept)/(l2.slope-m2.slope);
-  S.position.dy = (l2.slope*S.position.dx)+l2.intercept;
+  S.calcCircleDistance(B,context);
+  S.position.dx = (p.intercept-o.intercept)/(o.slope-p.slope);
+  S.position.dy = (o.slope*S.position.dx)+o.intercept;
   S.draw(context);
 }
 
