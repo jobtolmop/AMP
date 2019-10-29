31554ceaa51b45b26dd1945f74704ee0a9cf27c1
Jobau <jobtolmop@gmail.com>
2019-10-29 14:16:02 +0100

    Opdracht 11

--- html/11mousePointer/script.js
+++ html/11mousePointer/script.js
@@ -7,56 +7,14 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let  A = new Point(new Vector2d(100,110), 15, 255,0,0,true);
-let  B = new Point(new Vector2d(500,100), 15, 255,0,0,true);
-let  C = new Point(new Vector2d(300,500), 15, 255,0,0,true);
-let  D = new Point(new Vector2d(0,0), 5, 100,100,100,false);
-let  E = new Point(new Vector2d(0,0), 5, 100,100,100,false);
-let  F = new Point(new Vector2d(0,0), 5, 100,100,100,false);
-let  S = new Point(new Vector2d(0,0), 5, 200,200,200,false);
-
-let l = new LinearFunction(1,1);
-let m = new LinearFunction(1,1);
-let n = new LinearFunction(1,1);
-let o = new LinearFunction(1,1);
-let p = new LinearFunction(1,1);
-let q = new LinearFunction(1,1);
+let arrow = new Arrow(new Vector2d(width/2,height/2));
 
 function animate()
 {
-  window.requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-
-  l.defineLineByTwoPoints(A,B);
-  m.defineLineByTwoPoints(C,B);
-  n.defineLineByTwoPoints(C,A);
-
-  o.calcPerp(D,l);
-  p.calcPerp(F,m);
-  q.calcPerp(E,n);
-
-  l.draw(context);
-  m.draw(context);
-  n.draw(context);
-  o.draw(context);
-  p.draw(context);
-  q.draw(context);
-
-  D.calcMiddle(A,B);
-  E.calcMiddle(A,C);
-  F.calcMiddle(B,C);
-
-  A.draw(context);
-  B.draw(context);
-  C.draw(context);
-  D.draw(context);
-  E.draw(context);
-  F.draw(context);
-
-  S.calcCircleDistance(B,context);
-  S.position.dx = (p.intercept-o.intercept)/(o.slope-p.slope);
-  S.position.dy = (o.slope*S.position.dx)+o.intercept;
-  S.draw(context);
+  arrow.angle += 0.01;
+  arrow.draw(context);
 }
 
 animate();
