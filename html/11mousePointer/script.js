d2a72499dd96aaeaf05c44f368bdee6aa9a73a7a
Jobau <jobtolmop@gmail.com>
2019-11-01 14:33:01 +0100

    Finished 11

--- html/11mousePointer/script.js
+++ html/11mousePointer/script.js
@@ -7,14 +7,24 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+let mouse = {};
+
 let arrow = new Arrow(new Vector2d(width/2,height/2));
 
 function animate()
 {
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-  arrow.angle += 0.01;
+  let mx = mouse.x - arrow.pos.dx;
+  let my = mouse.y - arrow.pos.dy;
+  arrow.angle = Math.atan2(my, mx);
   arrow.draw(context);
 }
 
 animate();
+
+addEventListener('mousemove',(evt)=>
+{
+  mouse.x = evt.clientX;
+  mouse.y = evt.clientY;
+})
