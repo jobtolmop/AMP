3f1ca87f8e79bcccc1af3b8c115edb63299ccd8c
Jobau <jobtolmop@gmail.com>
2019-11-07 09:31:54 +0100

    Task 12 Done

--- html/12myCar/script.js
+++ html/12myCar/script.js
@@ -17,7 +17,9 @@ let wheelBack = new Image();
 wheelBack.src = "../../img/wheel.png";
 
 wheelBack.pos = new Vector2d(0,0);
+wheelBack.rotation = 0;
 wheelFront.pos = new Vector2d(0,0);
+wheelFront.rotation = 0;
 car.pos = new Vector2d(0,400);
 car.vel = new Vector2d(10,0);
 
@@ -34,13 +36,21 @@ function animate()
   context.drawImage(car,car.pos.dx,car.pos.dy);
   wheelFront.pos.dx = car.pos.dx;
   wheelFront.pos.dy = car.pos.dy;
-  wheelFront.pos.add(new Vector2d(674,120));
-  context.drawImage(wheelFront,wheelFront.pos.dx,wheelFront.pos.dy);
+  wheelFront.pos.add(new Vector2d(674 + (wheelFront.width/2),120 + (wheelFront.width/2)));
+  wheelFront.rotation += 0.1;
+  context.save();
+  context.translate(wheelFront.pos.dx, wheelFront.pos.dy);
+  context.rotate(wheelFront.rotation);
+  context.drawImage(wheelFront,-wheelFront.width/2,-wheelFront.width/2);
+  context.restore();
   wheelBack.pos.dx = car.pos.dx;
   wheelBack.pos.dy = car.pos.dy;
-  wheelBack.pos.add(new Vector2d(134,120));
-  context.rotate(wheelFront.rotate);
-  context.drawImage(wheelBack,wheelBack.pos.dx,wheelBack.pos.dy);
+  wheelBack.pos.add(new Vector2d(134 + (wheelBack.width/2),120 + (wheelBack.width/2)));
+  wheelBack.rotation += 0.1;
+  context.save();
+  context.translate(wheelBack.pos.dx, wheelBack.pos.dy);
+  context.rotate(wheelBack.rotation);
+  context.drawImage(wheelBack,-wheelBack.width/2,-wheelBack.width/2);
   context.restore();
   clamp();
 }
