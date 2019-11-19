b0b75017bd583f0e1d21944f914da337ff18ad97
Jobau <jobtolmop@gmail.com>
2019-11-19 14:30:53 +0100

    Start 13

---
+++ html/13drawTheVelocity/script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+let point = new DPoint(new Vector2d(200,200), new Vector2d(7,8), new Vector2d(0,0),20, "black");
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  context.clearRect(0,0,width,height);
+  point.update();
+  point.draw(context);
+  point.vel.draw(context,point.pos);
+}
+
+animate()
