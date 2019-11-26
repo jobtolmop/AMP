6727c0086cb2089606c1232502a68eb49a9e5580
Jobau <jobtolmop@gmail.com>
2019-11-26 14:06:16 +0100

    Finish 13

--- html/13drawTheVelocity/script.js
+++ html/13drawTheVelocity/script.js
@@ -7,15 +7,28 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
-let point = new DPoint(new Vector2d(200,200), new Vector2d(7,8), new Vector2d(0,0),20, "black");
+let points = [];
+
+for (var i = 0; i < 15; i++) {
+  points[i] = new DPoint(new Vector2d(GetRandom(400,50),GetRandom(400,50)), new Vector2d(GetRandom(8,2),GetRandom(4,3)), new Vector2d(0,0),20, "purple");
+}
 
 function animate()
 {
   requestAnimationFrame(animate);
   context.clearRect(0,0,width,height);
-  point.update();
-  point.draw(context);
-  point.vel.draw(context,point.pos);
+
+  for (var i = 0; i < points.length; i++) {
+    points[i].update();
+    points[i].draw(context);
+    points[i].vel.draw(context,points[i].pos, points[i].vel.angle);
+  }
+
+}
+
+function GetRandom(max, min)
+{
+  return Math.random()* max + min;
 }
 
 animate()
